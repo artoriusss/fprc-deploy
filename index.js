18568431d2a59b5b178f14f1403e9b0583e18fed
atroriusss <artorius.ua@gmail.com>
2024-03-25 08:55:33 +0200

    points on 4th level (not working properly yet)

--- index.js
+++ index.js
@@ -1,7 +1,84 @@
 let breadcrumbNames = ['Ukraine'];
 
+// const drilldown = async function (e) {
+//     //console.log('Drilling down into:', e.point.properties);
+//     if (!e.seriesOptions) {
+//         const chart = this;
+
+//         const isSecondLevel = e.point.drilldown.length > 4; 
+//         const isThirdLevel = e.point.drilldown.length > 6; 
+//         const isFourthLevel = e.point.drilldown.length > 9;
+
+//         let mapKey = 'adm-levels/';
+//         if (isFourthLevel) {
+//             mapKey += `ua-adm5/${e.point.drilldown}.geojson`;
+//         } else if (isThirdLevel) {
+//             mapKey += `ua-adm4/${e.point.drilldown}.geojson`;
+//         } else if (isSecondLevel) {
+//             mapKey += `ua-adm3/${e.point.drilldown}.geojson`;
+//         } else {
+//             mapKey += `ua-adm1/${e.point.drilldown}.geojson`;
+//         }
+
+
+//         chart.showLoading('<i class="icon-spinner icon-spin icon-3x"></i>');
+
+//         const topology = await fetch(mapKey).then(response => response.json());
+//         const data = Highcharts.geojson(topology);
+
+//         data.forEach((d, i) => {
+//             d.value = i;
+//             if (isFourthLevel) {
+//                 d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
+//             } 
+//             else if (isThirdLevel) {
+//                 d.drilldown = d.properties['ADM4_PCODE']; 
+//                 d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
+//             } else if (isSecondLevel) {
+//                 d.drilldown = d.properties['ADM3_PCODE']; 
+//                 d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
+//             } else {
+//                 d.drilldown = d.properties['ADM2_PCODE']; 
+//                 d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
+//             }
+//         });
+
+//         chart.hideLoading();
+
+//         // Set the series name based on the drilldown level and the clicked point information
+//         const seriesName = isFourthLevel
+//             ? e.point.properties['ADM4_EN']
+//             : isThirdLevel
+//             ? e.point.properties['ADM3_EN'] 
+//             : isSecondLevel
+//             ? e.point.properties['ADM2_EN'] 
+//             : e.point.properties['ADM1_EN']; 
+//         console.log('Series name:', seriesName);
+
+//         if (isFourthLevel) {
+//             breadcrumbNames = [seriesName];
+//         } else if (isThirdLevel) {
+//             breadcrumbNames = [seriesName];
+//         } else if (isSecondLevel) {
+//             breadcrumbNames = [seriesName];
+//         } else {
+//             breadcrumbNames = [seriesName];
+//         }
+
+//         console.log('Breadcrumb names:', breadcrumbNames);
+//         // Add or update the series for the drilldown
+//         chart.addSeriesAsDrilldown(e.point, {
+//             name: e.point.name, //e.point.properties['ADM1_EN'],
+//             data,
+//             dataLabels: {
+//                 enabled: true,
+//                 format: `{point.properties.${isThirdLevel ? 'ADM4_EN' : isSecondLevel ? 'ADM3_EN' : 'ADM2_EN'}}`
+//             }
+//         });
+//     }
+// };
+
 const drilldown = async function (e) {
-    //console.log('Drilling down into:', e.point.properties);
     if (!e.seriesOptions) {
         const chart = this;
 
@@ -20,63 +97,69 @@ const drilldown = async function (e) {
             mapKey += `ua-adm1/${e.point.drilldown}.geojson`;
         }
 
-
-        chart.showLoading('<i class="icon-spinner icon-spin icon-3x"></i>');
+        chart.showLoading('<i class="icon-spinner icon-spin icon-3x"></i>'); // Show loading icon
 
         const topology = await fetch(mapKey).then(response => response.json());
-        const data = Highcharts.geojson(topology);
+        let data = Highcharts.geojson(topology);
 
-        data.forEach((d, i) => {
-            d.value = i;
-            if (isFourthLevel) {
-                d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
-            } 
-            else if (isThirdLevel) {
-                d.drilldown = d.properties['ADM4_PCODE']; 
-                d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
-            } else if (isSecondLevel) {
-                d.drilldown = d.properties['ADM3_PCODE']; 
-                d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
-            } else {
-                d.drilldown = d.properties['ADM2_PCODE']; 
-                d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
-            }
-        });
+        chart.hideLoading(); // Hide loading icon
 
-        chart.hideLoading();
-
-        // Set the series name based on the drilldown level and the clicked point information
-        const seriesName = isFourthLevel
-            ? e.point.properties['ADM4_EN']
-            : isThirdLevel
-            ? e.point.properties['ADM3_EN'] 
-            : isSecondLevel
-            ? e.point.properties['ADM2_EN'] 
-            : e.point.properties['ADM1_EN']; 
-        console.log('Series name:', seriesName);
+        const seriesName = e.point.properties[`ADM${isFourthLevel ? '4' : isThirdLevel ? '3' : isSecondLevel ? '2' : '1'}_EN`];
+        breadcrumbNames.push(seriesName); // Update breadcrumb
 
         if (isFourthLevel) {
-            breadcrumbNames = [seriesName];
-        } else if (isThirdLevel) {
-            breadcrumbNames = [seriesName];
-        } else if (isSecondLevel) {
-            breadcrumbNames = [seriesName];
-        } else {
-            breadcrumbNames = [seriesName];
+        
+            // Transform geojson features into mappoint data for the individual points
+            const pointData = data.map(d => {
+                const latitude = parseFloat(d.properties['latitude'].replace(',', '.'));
+                const longitude = parseFloat(d.properties['longitude'].replace(',', '.'));
+                const amount = parseFloat(d.properties['amount']);
+        
+                if (!isNaN(latitude) && !isNaN(longitude) && !isNaN(amount)) {
+                    return {
+                        name: d.properties['ADM4_EN'],
+                        lat: latitude,
+                        lon: longitude,
+                        z: amount
+                    };
+                } else {
+                    return null;
+                }
+            }).filter(point => point !== null); // Filter out invalid points
+        
+            // Add the mappoint series with processed data
+            chart.addSeriesAsDrilldown(e.point, {
+                type: 'mappoint',
+                name: seriesName + ' - Points',
+                data: pointData,
+                color: Highcharts.getOptions().colors[1],
+                tooltip: {
+                    pointFormat: '<b>{point.name}</b><br>Lat: {point.lat}, Lon: {point.lon}, Amount: {point.z}'
+                }
+            });
+        
+            return; // Exit the function to prevent further processing
         }
+        else {
+            // Modify properties for areas
+            data.forEach((d, i) => {
+                d.value = i; // or any other value logic for your points
+                d.drilldown = d.properties[isThirdLevel ? 'ADM4_PCODE' : isSecondLevel ? 'ADM3_PCODE' : 'ADM2_PCODE'];
+                d.value = d.properties['amount'] === 0 ? 0.0001 : d.properties['amount'];
+            });
 
-        console.log('Breadcrumb names:', breadcrumbNames);
-        // Add or update the series for the drilldown
-        chart.addSeriesAsDrilldown(e.point, {
-            name: e.point.name, //e.point.properties['ADM1_EN'],
-            data,
-            dataLabels: {
-                enabled: true,
-                format: `{point.properties.${isThirdLevel ? 'ADM4_EN' : isSecondLevel ? 'ADM3_EN' : 'ADM2_EN'}}`
-            }
-        });
+            chart.addSeriesAsDrilldown(e.point, {
+                name: seriesName,
+                data: data,
+                dataLabels: {
+                    enabled: true,
+                    format: `{point.properties.${isThirdLevel ? 'ADM4_EN' : isSecondLevel ? 'ADM3_EN' : 'ADM2_EN'}}`
+                }
+            });
+        }
     }
 };
+
 
 // On drill up, reset to the top-level map view
 const afterDrillUp = function (e) {
